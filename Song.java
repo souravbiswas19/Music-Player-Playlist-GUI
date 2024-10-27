ffcdc894188213c0877eeff9befe306022320219
Sourav Biswas <souravbiswas19april@gmaail.com>
2024-10-27 11:53:06 +0530

    First Commit

---
+++ Song.java
@@ -0,0 +1,30 @@
+
+public class Song {
+
+    private String title;
+    private String artist;
+    private double duration;
+
+    public Song(String title, String artist, double duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Artist: " + artist + ", Duration: " + duration + " minutes";
+    }
+}
